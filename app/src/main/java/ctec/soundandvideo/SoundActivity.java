1eb2a23fef5c2d29128043962c2e27516010bbde
Brown, Ashton <abro5723@gmail.com>
2015-11-12 09:51:28 -0700

    Finished app

--- app/src/main/java/ctec/soundandvideo/SoundActivity.java
+++ app/src/main/java/ctec/soundandvideo/SoundActivity.java
@@ -10,20 +10,112 @@ import android.media.MediaPlayer;
 import android.view.View;
 import android.widget.*;
 
-public class SoundActivity extends AppCompatActivity
+public class SoundActivity extends Activity implements Runnable
 {
     private Button startButton;
     private Button stopButton;
     private Button pauseButton;
     private Button videoButton;
+    private MediaPlayer soundPlayer;
     private SeekBar soundSeekBar;
     private Thread soundThread;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState)
-    {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sound);
+
+        startButton = (Button) findViewById(R.id.playButton);
+        pauseButton = (Button) findViewById(R.id.pauseButton);
+        stopButton = (Button) findViewById(R.id.stopButton);
+        soundSeekBar = (SeekBar) findViewById(R.id.soundSeekBar);
+        videoButton = (Button) findViewById(R.id.videoButton);
+        //soundPlayer = MediaPlayer.create(this.getBaseContext(), R.raw.pomdeter);
+
+        setupListeners();
+
+        soundThread = new Thread(this);
+        soundThread.start();
+    }
+
+    private void setupListeners()
+    {
+        startButton.setOnClickListener(new View.OnClickListener()
+        {
+           public void onClick(View v)
+           {
+               soundPlayer.start();
+           }
+        });
+
+        pauseButton.setOnClickListener(new View.OnClickListener()
+        {
+            public void onClick(View v)
+            {
+                soundPlayer.pause();
+            }
+
+        });
+
+        stopButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View currentView) {
+                soundPlayer.stop();
+                soundPlayer = MediaPlayer.create(getBaseContext(), R.raw.pomdeter);
+            }
+
+
+        });
+
+        videoButton.setOnClickListener(new View.OnClickListener()
+        {
+            public void onClick(View currentView)
+            {
+                Intent myIntent = new Intent(currentView.getContext(), VideoActivity.class);
+                startActivityForResult(myIntent, 0);
+            }
+        });
+
+        soundSeekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            public void onStopTrackingTouch(SeekBar seekbar) {
+            }
+
+            public void onStartTrackingTouch(SeekBar seekBar) {
+            }
+
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                if (fromUser) {
+                    soundPlayer.seekTo(progress);
+                }
+            }
+
+        });
+
+    }
+
+    public void run()
+    {
+        int currentPosition = 0;
+        int soundTotal = soundPlayer.getDuration();
+        soundSeekBar.setMax(soundTotal);
+
+        while (soundPlayer != null && currentPosition < soundTotal)
+        {
+            try
+            {
+                Thread.sleep(300);
+                currentPosition = soundPlayer.getCurrentPosition();
+            }
+            catch(InterruptedException soundException)
+            {
+                return;
+            }
+            catch(Exception otherException)
+            {
+                return;
+            }
+            soundSeekBar.setProgress(currentPosition);
+
+        }
     }
 
     @Override
--- app/src/main/java/ctec/soundandvideo/VideoActivity.java
+++ app/src/main/java/ctec/soundandvideo/VideoActivity.java
@@ -1,7 +1,57 @@
 package ctec.soundandvideo;
 
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.*;
+import android.net.Uri;
+
 /**
  * Created by abro5723 on 11/10/15.
  */
-public class VideoActivity {
+public class VideoActivity extends Activity
+{
+    private VideoView myPlayer;
+    private Button returnButton;
+    private MediaController myVideoController;
+    private Uri videoLocation;
+
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_video);
+
+        myPlayer = (VideoView) findViewById(R.id.videoView);
+        returnButton = (Button) findViewById(R.id.homeButton);
+
+        videoLocation = Uri.parse("android.resource://" + getPackageName() + "/" + R.raw.party);
+        myVideoController = new MediaController(this);
+
+        setupMedia();
+        setupListeners();
+    }
+
+    private void setupMedia()
+    {
+        myPlayer.setMediaController(myVideoController);
+        myPlayer.setVideoURI(videoLocation);
+    }
+
+    private void setupListeners()
+    {
+        returnButton.setOnClickListener(new View.OnClickListener()
+        {
+            public void onClick(View currentView)
+            {
+                Intent returnIntent = new Intent();
+                setResult(RESULT_OK, returnIntent);
+                finish();
+
+            }
+
+        });
+    }
 }
